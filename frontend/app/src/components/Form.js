5afdfa7349cb5d0c8e158adbe23b604183bb94ce
kamesan1577 <kamesan1577@users.noreply.github.com>
2023-07-21 04:59:50 +0900

    フォームの修正

--- frontend/app/src/components/Form.js
+++ frontend/app/src/components/Form.js
@@ -10,70 +10,72 @@ function Form() {
     const [lhs, setLhs] = useState(0);
     const [rhs, setRhs] = useState(0);
     const [operand, setOpe] = useState("+");
-    function Culc(props) {
+
+    function Culc() {
+        console.log();
         axios
             .post("http://localhost:8080/culc",
                 {
-                    "num1": props.num1,
-                    "num2": props.num2,
-                    "ope": props.ope,
-                })
+                    "num1": lhs,
+                    "num2": rhs,
+                    "ope": operand,
+                }, { timeout: 1000 })
             .then((res) => {
                 setValue(res.result);
             })
             .catch((err) => {
-                console.log(`${props.num1} ${props.ope} ${props.num2} = ??`)
-                console.log("通信エラー")
+                console.log(`${lhs} ${operand} ${rhs} = ??`);
+                console.log("通信エラー");
             })
     }
+
     return (
-        <Table hover striped bordered>
-            <thead>
-                <tr>
-                    <th>左辺</th>
-                    <th>右辺</th>
-                    <th>演算子</th>
-                    <th>結果</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        <input type="number" value={lhs} onChange={(event) => setLhs(event.target.value)} placeholder="0"></input>
-                    </td>
-                    <td>
-                        <input type="number" value={rhs} onChange={(event) => setRhs(event.target.value)} placeholder="0"></input>
-                    </td>
-                    <td>
-                        <Container>
-                            <Row xs="4">
-                                <Col>
-                                    <Button variant="outline-secondary" onClick={() => setOpe("+")}>+</Button>
-                                </Col>
-                                <Col>
-                                    <Button variant="outline-secondary" onClick={() => setOpe("-")}>-</Button>
-                                </Col>
-                                <Col>
-                                    <Button variant="outline-secondary" onClick={() => setOpe("*")}>*</Button>
-                                </Col>
-                                <Col>
-                                    <Button variant="outline-secondary" onClick={() => setOpe("/")}>/</Button>
-                                </Col>
-
-                            </Row>
-                        </Container>
-                    </td>
-                    <td>
-                        <p>{value}</p>
-                    </td>
-                    <td>
-                        <Button variant="outline-secondary" num1={lhs} num2={rhs} ope={operand} onClick={Culc}>計算</Button>
-                    </td>
-
-
-                </tr>
-            </tbody>
-        </Table >
+        <Container>
+            <Table hover striped bordered>
+                <thead>
+                    <tr>
+                        <th>左辺</th>
+                        <th>右辺</th>
+                        <th>演算子</th>
+                        <th>結果</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input type="number" value={lhs} onChange={(event) => setLhs(event.target.value)} placeholder="0"></input>
+                        </td>
+                        <td>
+                            <input type="number" value={rhs} onChange={(event) => setRhs(event.target.value)} placeholder="0"></input>
+                        </td>
+                        <td>
+                            <Container>
+                                <Row xs="4">
+                                    <Col>
+                                        <Button variant="outline-secondary" onClick={() => setOpe("+")}>+</Button>
+                                    </Col>
+                                    <Col>
+                                        <Button variant="outline-secondary" onClick={() => setOpe("-")}>-</Button>
+                                    </Col>
+                                    <Col>
+                                        <Button variant="outline-secondary" onClick={() => setOpe("*")}>*</Button>
+                                    </Col>
+                                    <Col>
+                                        <Button variant="outline-secondary" onClick={() => setOpe("/")}>/</Button>
+                                    </Col>
+                                </Row>
+                            </Container>
+                        </td>
+                        <td>
+                            <p>{value}</p>
+                        </td>
+                        <td>
+                            <Button variant="outline-secondary" num1={lhs} num2={rhs} ope={operand} onClick={Culc}>計算</Button>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table >
+        </Container>
     );
 }
 
